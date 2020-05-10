07457756ca70aa75c746f778f53ebfcfebf21ac1
Dmitry Shatilov <shatilov.diman@gmail.com>
2020-05-10 22:21:09 +0300

    force

--- src/modules/lobby.ts
+++ src/modules/lobby.ts
@@ -1,81 +1,44 @@
 import * as io from 'socket.io-client';
 
 class Lobby {
-    users = {}
-    matches = {}
-
     socket = null
-    lobby = null
 
     handlers = {}
+    players = {}
 
     constructor() {
-        this.loadLobby();
     }
 
-    private loadLobby() {
-        console.log('connecting');
-        this.socket = io.connect("https://mars-racing.herokuapp.com");
-        this.lobby = io.connect("https://mars-racing.herokuapp.com/lobby");
-        this.lobby.on("connect", () => {
-            this.users = {};
-            this.matches = {};
-            console.log('on connect to lobby');
+    public start(nickname) {
+        const racing_server = process.env.RACING_SERVER || "localhost:8081";
+        console.log(`connecting to '${racing_server}'`);
 
-            this.trigger('connect');
+        this.socket = io.connect(racing_server);
+        this.socket.on('connect', () => {
+            this.socket.emit('broadcast', {
+                type: 'hello',
+                nickname: nickname,
+            })
         });
+        this.socket.on('buy', (msg) => {
+            this.trigger('buy', msg)
+            delete this.players[msg.sid]
+        });
+        this.socket.on('broadcast', (msg) => {
+            if (msg.type === 'hello') {
+                this.players[msg.sid] = msg
+                this.trigger('hello', msg)
+            } else {
+                this.trigger('msg', msg)
+            }
+        });
+        this.socket.emit('users', (users) => {
+            this.trigger('users', users)
+        });
+    }
 
-	this.lobby.on('userEnter', (uid, user) => {
-		console.log('userEnter', uid, user);
-		this.users[uid] = user;
-                this.trigger('userEnter', user);
-	});
-	this.lobby.on('userLeave', (uid, user) => {
-		console.log('userLeave', uid, user);
-                this.trigger('userLeave', user);
-	});
-
-	this.lobby.on('matchAdd', (match) => {
-		console.log('matchAdd', match);
-		this.matches[match.id] = match;
-                this.trigger('matchAdd', match);
-	});
-
-	this.lobby.on('matchUpdated', (match) => {
-		console.log('matchUpdated', match);
-		this.matches[match.id] = match;
-                this.trigger('matchUpdated', match);
-	});
-
-	this.lobby.on('matchJoined', (mid, uid) => {
-		console.log('matchJoined', mid, uid);
-		this.matches[mid].players[uid] = false;
-                this.trigger('matchJoined', [this.matches[mid], this.users[uid]]);
-	});
-
-	this.lobby.on('matchReady', (mid, uid) => {
-		console.log('matchReady', mid, uid);
-		this.matches[mid].players[uid] = true;
-                this.trigger('matchReady', [this.matches[mid], this.users[uid]]);
-	});
-
-	this.lobby.on('matchLeaved', (mid, uid) => {
-		console.log('matchLeaved', mid, uid)
-		delete this.matches[mid].players[uid]
-                this.trigger('matchLeaved', [this.matches[mid], this.users[uid]]);
-	});
-
-	this.lobby.on('matchStarted', (mid) => {
-		console.log('matchStarted', mid);
-		this.matches[mid].status = 'playing';
-                this.trigger('matchStarted', this.matches[mid]);
-	});
-
-	this.lobby.on('matchFinished', (mid) => {
-		console.log('matchFinished', mid);
-                this.trigger('matchFinished', this.matches[mid]);
-		delete this.matches[mid];
-	});
+    public send(msg) {
+        this.socket.emit('broadcast', msg)
     }
 
     public on(event:string, handler: {(data?:any):void;}) {
@@ -92,50 +55,8 @@ class Lobby {
             this.handlers[event].forEach(h => h(data));
     }
 
-    public getUser(uid) {
-        return this.users[uid]
-    }
-
     public getPlayers(mid) {
-        if (mid in this.matches)
-            return this.matches[mid].players.keys().map(uid => this.getUser(uid));
-        return []
-    }
-
-    public signIn(uid, nickname) {
-        console.log('sign-in', uid);
-        this.lobby.emit('sign-in', uid, nickname, (welcome) => {
-            console.log(welcome);
-            this.trigger('signed-in');
-        });
-    }
-
-    public createNewGame(settings) {
-        console.log('matchNew', settings);
-        this.lobby.emit('matchNew', settings, (result) => {
-            console.log(result)
-        })
-    }
-
-    public joinGame(mid) {
-        console.log('matchJoin', mid);
-	this.lobby.emit('matchJoin', mid, (result) => {
-            console.log(result)
-	})
-    }
-
-    public readyToPlay(mid) {
-        console.log('matchReady', mid);
-	this.lobby.emit('matchReady', mid, (result) => {
-            console.log(result)
-	})
-    }
-
-    public leaveGame(mid) {
-        console.log('matchLeave', mid);
-	this.lobby.emit('matchLeave', mid, (result) => {
-            console.log(result)
-	})
+        return Object.keys(this.players).map(uid => this.players[uid])
     }
 }
 
